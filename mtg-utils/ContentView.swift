1505d1ba172a731a3aff634bf85a43d3037d557b
JLPB-CODE <jlpordomingobrito@gmail.com>
2026-09-06 21:11:48 +0100

    Initial Commit

---
+++ mtg-utils/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  mtg-utils
+//
+//  Created by Jose Luis on 06/09/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ mtg-utils/mtg_utilsApp.swift
@@ -0,0 +1,17 @@
+//
+//  mtg_utilsApp.swift
+//  mtg-utils
+//
+//  Created by Jose Luis on 06/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct mtg_utilsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
